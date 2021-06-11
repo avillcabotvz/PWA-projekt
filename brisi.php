62aeac8c5654b48c57e57edc4b542e0ba3bff05c
Andrey Inti Villca Bozicevic <avillcabo@tvz.hr>
2021-06-11 23:58:56 +0200

    Dodano brisanje zapisa

---
+++ brisi.php
@@ -0,0 +1,8 @@
+<?php
+
+  $id = $_GET['id'];
+  $dbc = mysqli_connect("localhost", "root", "", "pwa_projekt") or die('Could not connect: ' . mysqli_connect_error());
+  $query = "DELETE FROM clanak where id = '$id'";
+  $result = mysqli_query($dbc, $query);
+
+  echo "<script type='text/javascript'> document.location = 'brisanje.php'; </script>";
